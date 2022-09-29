f6f4ff2390124254579eb62bdad0cd12883a4b05
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-29 22:16:17 +0200

    Part 3.4 done

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express")
 
 const store = require("./store/persons.json")
+let persons = store.persons
 const app = express()
 
 
+app.get("/info", (request, response) => {
+  const now = new Date()
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${now}</p>`
+  )
+})
+
 app.get("/api/persons", (request, response) => {
-  response.json(store.persons)
+  response.json(persons)
 })
 
 app.get("/api/persons/:id", (request, response) => {
     const id = Number(request.params.id)
-    const person = store.persons.find(p => p.id === id)
+    const person = persons.find(p => p.id === id)
 
     if (person) {
         response.json(person)
@@ -19,12 +28,11 @@ app.get("/api/persons/:id", (request, response) => {
     }
 })
 
-app.get("/info", (request, response) => {
-  const now = new Date()
-  response.send(
-    `<p>Phonebook has info for ${store.persons.length} people</p>
-    <p>${now}</p>`
-  )
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    response.end()
 })
 
 const PORT = 3001
