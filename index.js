ff5568e6430b50eddfc94a04ee3caab4f7dd36af
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-29 22:39:07 +0200

    Part 3.5 done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const store = require("./store/persons.json")
 let persons = store.persons
 const app = express()
 
+app.use(express.json())
 
 app.get("/info", (request, response) => {
   const now = new Date()
@@ -18,21 +19,39 @@ app.get("/api/persons", (request, response) => {
 })
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find((p) => p.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post("/api/persons", (request, response) => {
+  if (request.body["name"] && request.body["phone"]) {
+    if (persons.find((p) => p.name === request.body.name)) {
+      return response.status(409).json({ error: "this person already exists!" })
     }
+    const newPerson = {
+      name: request.body.name,
+      phone: request.body.phone,
+      id: Math.floor(Math.random() * 99999999999999999),
+    }
+    persons.push(newPerson)
+
+    return response.json(newPerson)
+  } else {
+    return response.status(400).json({ error: "invalid arguments. Should be 'name' and 'phone'" })
+  }
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
+  const id = Number(request.params.id)
+  persons = persons.filter((p) => p.id !== id)
 
-    response.end()
+  response.end()
 })
 
 const PORT = 3001
