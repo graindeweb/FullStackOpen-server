fa6d7a6ab5a980902f5fa53ddf12caf93f501c8e
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-29 22:04:49 +0200

    Part 3.2 done

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get("/api/persons", (request, response) => {
   response.json(store.persons)
 })
 
+app.get("/info", (request, response) => {
+  const now = new Date()
+  response.send(
+    `<p>Phonebook has info for ${store.persons.length} people</p>
+    <p>${now}</p>`
+  )
+})
 
 const PORT = 3001
 
